121e573e86b5b1049707bdc4a28d97140f1991fd
sreelakshmi p s <srelakshmi0777@gmail.com>
2022-05-15 04:54:05 +0530

    initial stage 2.0

--- HW_Task/HW_Task/spiders/scraper.py
+++ HW_Task/HW_Task/spiders/scraper.py
@@ -1,6 +1,4 @@
 import scrapy
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
 
 base = 'https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page={}'
 
@@ -10,47 +8,17 @@ class OlxScrap(scrapy.Spider):
     start_urls = [base.format(1)]
 
     def parse(self, response):
-
-        for houses in response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']"):
-            try:
-                yield {
-                    'name': response.css('span._2tW1I::text').get(),
-                    'price': response.css('span._89yzn::text').get(),
-                    'ft': response.css('span._2TVI3::text').get(),
-                    'place': response.css('span.tjgMj::text').get(),
-                    'link': response.css('a.fhlkh').attrib['href']
-                }
-
-                next_page = base.format()
-                yield scrapy.Request(next_page)
-            except:
-                print("error")
+        for house in response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']"):
+            # try:
+            yield {
+                'figure': house.xpath("//a[@class='fhlkh']/figure[@class='_2grx4']").get(),
+                "price": house.xpath("//div[@class='IKo3_']/span[@class='_89yzn']/text()").get(),
+                "ft": house.xpath("//div[@class='IKo3_']/span[@class='_2TVI3']/text()").get(),
+                "name": house.xpath("//div[@class='IKo3_']/span[@class='_2tW1I']/text()").get(),
+                "place": house.xpath("//div[@class='_1KOFM']/span[@class='tjgMj']/text()").get(),
+            }
 
 
-class Olx_Scraper(CrawlSpider):
-    name = 'olxs'
-    start_urls = ['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page={}']
-    link_extract = LinkExtractor(restrict_xpaths='//ul/li')
-    rule_extract = Rule(link_extract,
-                        callback='parse_item',
-                        follow=False)
-    rules = (
-        rule_extract
-    )
-
-    def parse_item(self, response):
-        yield {
-            'property_name': response.xpath('//section/h1/text()').extract(),
-            'property_id': response.xpath('//div/strong/text()').extract(),
-            'breadcrumbs':response.xpath('')
-        }
-
-# next_page =
-# def parse(self, response):
-#     for house in response.css('li.EIR5N'):
-# def parse(self, response):
-#     for houses in response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']"):
-#         pass
-# response.xpath("//div[@class='IKo3_']/span[@class='_2tW1I']").extract()
-# houses.xpath("//div[@class='IKo3_']/span[@class='_2tW1I']/text()").getall()
-# house =  response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']")
+        # except:
+        #     print("error")
+# //*[@id="container"]/main/div/div/section/div/div/div[5]/div[2]/div/div[2]/ul/li[1]/a
