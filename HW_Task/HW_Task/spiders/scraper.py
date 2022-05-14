6fc1330a414c3060e36b00d0eefa1f59ef542591
sreelakshmi p s <srelakshmi0777@gmail.com>
2022-05-15 05:27:30 +0530

    initial stage 2.1

--- HW_Task/HW_Task/spiders/scraper.py
+++ HW_Task/HW_Task/spiders/scraper.py
@@ -1,11 +1,17 @@
 import scrapy
 
-base = 'https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page={}'
-
 
 class OlxScrap(scrapy.Spider):
     name = 'olx'
-    start_urls = [base.format(1)]
+    base_url = 'https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page='
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36'
+    }
+
+    def start_requests(self):
+        for page in range(0, 28):
+            next_page = self.base_url + str(page)
+            yield scrapy.Request(url=next_page, headers=self.headers, callback=self.parse)
 
     def parse(self, response):
         for house in response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']"):
@@ -18,7 +24,4 @@ class OlxScrap(scrapy.Spider):
                 "place": house.xpath("//div[@class='_1KOFM']/span[@class='tjgMj']/text()").get(),
             }
 
-
-        # except:
-        #     print("error")
 # //*[@id="container"]/main/div/div/section/div/div/div[5]/div[2]/div/div[2]/ul/li[1]/a
