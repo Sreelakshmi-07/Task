47d6697b3a8ec641288a73ac45c7daf2985216ff
sreelakshmi p s <srelakshmi0777@gmail.com>
2022-05-15 11:05:50 +0530

    initial stage 2.2

--- HW_Task/HW_Task/spiders/scraper.py
+++ HW_Task/HW_Task/spiders/scraper.py
@@ -1,7 +1,10 @@
 import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+import json
 
 
-class OlxScrap(scrapy.Spider):
+class Olx_Scrap(scrapy.Spider):
     name = 'olx'
     base_url = 'https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page='
     headers = {
@@ -9,13 +12,12 @@ class OlxScrap(scrapy.Spider):
     }
 
     def start_requests(self):
-        for page in range(0, 28):
+        for page in range(1, 28):
             next_page = self.base_url + str(page)
             yield scrapy.Request(url=next_page, headers=self.headers, callback=self.parse)
 
     def parse(self, response):
         for house in response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']"):
-            # try:
             yield {
                 'figure': house.xpath("//a[@class='fhlkh']/figure[@class='_2grx4']").get(),
                 "price": house.xpath("//div[@class='IKo3_']/span[@class='_89yzn']/text()").get(),
@@ -23,5 +25,31 @@ class OlxScrap(scrapy.Spider):
                 "name": house.xpath("//div[@class='IKo3_']/span[@class='_2tW1I']/text()").get(),
                 "place": house.xpath("//div[@class='_1KOFM']/span[@class='tjgMj']/text()").get(),
             }
+
+
+class Olx_Items(CrawlSpider):
+    name = "items"
+    allowed_domains = ['www.olx.in']
+    start_urls = ['https://www.olx.in/kozhikode_g4058877/for-rent-houses-apartments_c1723?page=']
+    rules = (
+        Rule(LinkExtractor(allow='kozhikode_g4058877/for-rent-houses-apartments_c1723',deny='item')),
+        Rule(LinkExtractor(allow='item'),callback='parse_item')
+    )
+    def parse_item(self,response):
+        yield {
+            'product':response.xpath("//div[@class='rui-2CYS9']/section[@class='_2wMiF']/h1[@class='_3rJ6e']/text()").get()
+        }
+
+
+
+
+
+# def parse_item(self,response):
+#     for houses in response.xpath("//ul[@class='rl3f9 _3mXOU']/li[@class='EIR5N']/a[@class='fhlkh']/@href"):
+#         yield {
+#             'property_name': houses.xpath('//section/h1/text()').extract(),
+#             'property_id': houses.xpath('//div/strong/text()').extract(),
+#             # 'breadcrumbs': houses.xpath('')
+#         }
 
 # //*[@id="container"]/main/div/div/section/div/div/div[5]/div[2]/div/div[2]/ul/li[1]/a
